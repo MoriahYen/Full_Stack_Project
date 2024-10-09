adac12fdeaf36f351534a25c6849ade1a8ea3891
moriahyen <moriahyen@gmail.com>
2024-10-09 17:41:23 +0800

    First commit.

---
+++ backend/fullstackweb/src/main/java/com/moriahyen/fullstackweb/FullstackwebApplication.java
@@ -0,0 +1,13 @@
+package com.moriahyen.fullstackweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackwebApplication.class, args);
+	}
+
+}
