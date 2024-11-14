9fc95a2cecc8d844869d3f7d9d7c65af191ce04e
moriahyen <moriahyen@gmail.com>
2024-11-14 16:40:13 +0800

    111. Angular Project - Pagination - Update Product List Component for Pagination

--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -24,6 +24,17 @@ export class ProductService {
     return this.httpClient.get<Product>(productUrl);
   }
 
+  getProductListPaginate(thePage: number, 
+                         thePageSize: number, 
+                         theCategoryId: number): Observable<GetResponseProducts> {
+
+    // need to build URL based on category id, page and size 
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+                    + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   // [M] Return an ocservable Map the JSON from Spring Data REST to Product array
   getProductList(theCategoryId: number): Observable<Product[]> {
 
@@ -59,6 +70,12 @@ export class ProductService {
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
 }
 
