652c5aba6668318c80b01ba2a755f7457affacc3
moriahyen <moriahyen@gmail.com>
2024-11-21 16:57:32 +0800

    183. Angular Project - Checkout Form - Custom Validator Rule - Define the Rule

---
+++ frontend/angular-ecommerce/src/app/validators/luv2-shop-validatora.spec.ts
@@ -0,0 +1,7 @@
+import { Luv2ShopValidatora } from './luv2-shop-validatora';
+
+describe('Luv2ShopValidatora', () => {
+  it('should create an instance', () => {
+    expect(new Luv2ShopValidatora()).toBeTruthy();
+  });
+});
