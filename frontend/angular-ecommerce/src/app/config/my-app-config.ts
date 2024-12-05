286850c55e1c6d2312c87fb9f2b39960c1aae632
moriahyen <moriahyen@gmail.com>
2024-12-05 18:59:32 +0800

    226. Security - Login/Logout - Coding - Develop App Configuration

---
+++ frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oalmfqwnoO63eHXv5d7',
+        issuer: 'https://dev-54177175.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
