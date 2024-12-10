cc0985ee0ae943ff1012da68ea346d585e6c2bb2
moriahyen <moriahyen@gmail.com>
2024-12-10 16:00:35 +0800

    "Fail" OrderHistory no show. 262. Release 3.0 - Security - Order History - Frontend Coding - Sort Orders by Date

--- backend/fullstackweb/src/main/java/com/luv2code/fullstackweb/controller/CheckoutController.java
+++ backend/fullstackweb/src/main/java/com/luv2code/fullstackweb/controller/CheckoutController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/checkout")
+@CrossOrigin("http://localhost:4200")
 public class CheckoutController {
 
     private CheckoutService checkoutService;
