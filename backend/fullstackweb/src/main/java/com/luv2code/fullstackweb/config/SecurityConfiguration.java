7c6c16ee3fc16befb588eae0dced440af42923b5
moriahyen <moriahyen@gmail.com>
2024-12-11 16:33:53 +0800

    Fixed error.

--- backend/fullstackweb/src/main/java/com/luv2code/fullstackweb/config/SecurityConfiguration.java
+++ backend/fullstackweb/src/main/java/com/luv2code/fullstackweb/config/SecurityConfiguration.java
@@ -8,33 +8,33 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.accept.ContentNegotiationStrategy;
 import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        // protect endpoint /api/orders
-        http.authorizeHttpRequests(configurer ->
-                        configurer
+        //protect endpoint /api/orders
+        http.authorizeHttpRequests(requests ->
+                        requests
                                 .requestMatchers("/api/orders/**")
-                                .authenticated().anyRequest().permitAll())
-                .oauth2ResourceServer()
-                .jwt();
+                                .authenticated()
+                                .anyRequest().permitAll())
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()));
 
-        // add CORS filters
-        http.cors();
+        // + CORS filters
+        http.cors(Customizer.withDefaults());
 
-        // add content negotiation strategy
-        http.setSharedObject(ContentNegotiationStrategy.class,
-                new HeaderContentNegotiationStrategy());
+        // + content negotiation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
 
-        // force a non-empty response body for 401's to make the response more friendly
+        // + non-empty response body for 401 (more friendly)
         Okta.configureResourceServer401ResponseBody(http);
 
-        // disable CSRF since we are not using Cookies for session tracking
-        http.csrf().disable();
+        // we are not using Cookies for session tracking >> disable CSRF
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
