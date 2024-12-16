b215927ba2407dddbd2768fca8c3c98fb3d20ac6
moriahyen <moriahyen@gmail.com>
2024-12-16 22:49:57 +0800

    https://localhost:4200/products fail. https://localhost:8443/api/products works. 286. Angular Environment Configuration - Coding

--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Product } from '../common/product';
 import { ProductCategory } from '../common/product-category';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,9 @@ import { ProductCategory } from '../common/product-category';
 export class ProductService {
     
   // [M] server
-  private baseUrl = 'http://localhost:8080/api/products';
-  private categoryUrl = 'http://localhost:8080/api/product-category';
+  private baseUrl = environment['luv2shopApiUrl'] + '/products';
+
+  private categoryUrl = environment['luv2shopApiUrl'] + '/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
