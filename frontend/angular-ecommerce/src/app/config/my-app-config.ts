a385082371667da970c73dacba71728b95bff65e
moriahyen <moriahyen@gmail.com>
2024-12-16 22:17:10 +0800

    280. Secure Communication with HTTPS - Okta Updates - Configuration

--- frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oalmfqwnoO63eHXv5d7',
         issuer: 'https://dev-54177175.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 
