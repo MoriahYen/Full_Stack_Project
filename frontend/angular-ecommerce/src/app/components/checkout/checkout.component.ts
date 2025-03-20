d2dfebfb50ca3527fc2fe4dacd5e299c4953ccba
moriahyen <moriahyen@gmail.com>
2025-03-20 19:29:13 +0800

    311. Payment Processing - Stripe - Frontend Coding - Part 6

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -135,9 +135,31 @@ export class CheckoutComponent implements OnInit {
       }
     );
   }
-  
+
   setupStripePaymentForm() {
-    throw new Error('Method not implemented.');
+    // get a handle to stripe elements
+    var elements = this.stripe.elements();
+
+    // Create a card element ... and hide the zip-code field
+    this.cardElement = elements.create('card', { hidePostalCode: true });
+
+    // Add an instance of card UI component into the 'card-element' div
+    this.cardElement.mount('#card-element');
+
+    // Add event binding for the 'change' event on the card element
+    this.cardElement.on('change', (event) => {
+
+      // get a handle to card-errors element
+      this.displayError = document.getElementById('card-errors');
+
+      if (event.complete) {
+        this.displayError.textContent = "";
+      } else if (event.error) {
+        // show validation error to customer
+        this.displayError.textContent = event.error.message;
+      }
+
+    });
   }
 
   reviewCartDetails() {
