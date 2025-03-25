f87543453822237cf5eb10c499c22138ae6990bb
moriahyen <moriahyen@gmail.com>
2025-03-26 03:46:42 +0800

    Refactored backend.

---
+++ backend/fullstackweb/src/main/java/com/luv2code/ecommerce/FullstackwebApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackwebApplication.class, args);
+	}
+
+}
--- backend/fullstackweb/src/main/java/com/luv2code/fullstackweb/service/CheckoutService.java
+++
@@ -1,8 +0,0 @@
-package com.luv2code.fullstackweb.service;
-
-import com.luv2code.fullstackweb.dto.Purchase;
-import com.luv2code.fullstackweb.dto.PurchaseResponse;
-
-public interface CheckoutService {
-    PurchaseResponse placeOrder(Purchase purchase);
-}
